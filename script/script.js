4e0aaa999e517e305617f9820cd913bf441d6e93
Max Kuznetsov <maxkuznec12@gmail.com>
2020-03-26 21:40:40 +0300

    Первая домашка

---
+++ script/script.js
@@ -0,0 +1,13 @@
+let money = 5, 
+    income = 'Hi', 
+    addExpenses = {
+        a: 1,
+        b: 'js'
+    }, 
+    deposit = null, 
+    mission = [1, 2, 3], 
+    period = true;
+
+    console.log(addExpenses);
+
+    alert('Не первый мой алерт.');
