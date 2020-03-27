a7f94c534c07cf92a59cd447ad70301854fe0942
Max Kuznetsov <maxkuznec12@gmail.com>
2020-03-27 20:35:06 +0300

    Добавил вторую домашку

--- script/script.js
+++ script/script.js
@@ -1,5 +1,15 @@
-let money, income, addExpenses, deposit, mission, period;
+let money = 25000, 
+    income = 'фриланс', 
+    addExpenses = 'квартира, Еда, подарки', 
+    deposit = false, 
+    mission = 500000, 
+    period = 3;
 
-console.log('Исправил ошибки');
+console.log(typeof money, typeof income, typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяца. Цель заработать ' + mission + ' рублей.');
+console.log(addExpenses.toLowerCase().split(', '));
 
-alert('Не первый мой алерт.');
+let budgetDay = money / 30;
+
+console.log(budgetDay);
