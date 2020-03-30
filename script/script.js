c36a5ee72033ee184e4f39eb6016efefbee51886
Max Kuznetsov <maxkuznec12@gmail.com>
2020-03-30 23:10:10 +0300

    Работа по 3й домашке

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let money = 25000, 
     income = 'фриланс', 
     addExpenses = 'квартира, Еда, подарки', 
@@ -13,3 +15,31 @@ console.log(addExpenses.toLowerCase().split(', '));
 let budgetDay = money / 30;
 
 console.log(budgetDay);
+
+money = prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = Boolean(confirm('Есть ли у вас депозит в банке?'));
+
+let expensesOne = prompt('Введите обязательную статью расходов?');
+let amountOne = +prompt('Во сколько это обойдется?');
+
+let expensesTwo = prompt('Введите обязательную статью расходов?');
+let amountTwo = +prompt('Во сколько это обойдется?');
+
+let budgetMonth = money - (amountOne + amountTwo);
+console.log('Бюджет на месяц: ' + budgetMonth);
+
+console.log('Цель будет достигнута за: ' + Math.ceil(mission / budgetMonth));
+
+budgetDay = budgetMonth / 30;
+console.log('Бюджет на день: ' + Math.floor(budgetDay));
+
+if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay > 600 && budgetDay < 1200) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay <= 600 || budgetDay === 0) {
+    console.log('К сожалению, у вас уровень дохода ниже среднего');
+} else {
+    console.log('Что то пошло не так');
+}
