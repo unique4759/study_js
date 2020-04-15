83a8fa6288c2a84e022535d7c26830839d006ec8
Max Kuznetsov <maxkuznec12@gmail.com>
2020-04-16 00:43:18 +0300

    14 усложненка

--- script/with_class.js
+++ script/with_class.js
@@ -1,42 +1,72 @@
 'use strict';
 
-function DomElement(selector, height, width, bg, fontSize) {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-};
+document.addEventListener("DOMContentLoaded", () => {
+    function DomElement(selector, height, width, bg, fontSize) {
+        this.selector = selector;
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.fontSize = fontSize;
+    };
+    
+    DomElement.prototype.createEl = function() {
+        let firstSymbol = this.selector[0],
+            bodyElement;
+    
+        if(firstSymbol === '.') {
+            let div = document.createElement('div');
+    
+            div.textContent = this.fontSize ? 'Класс ' + this.selector.slice(1) : '';
+            div.classList.add(this.selector.slice(1));
+            document.body.appendChild(div);
+            bodyElement = div;
+        }
+    
+        if(firstSymbol === '#') {
+            let paragraph = document.createElement('p');
+    
+            paragraph.textContent = this.fontSize ? 'Айдишник ' + this.selector.slice(1) : 0;
+            paragraph.setAttribute('id', this.selector.slice(1));
+            document.body.appendChild(paragraph);
+            bodyElement = paragraph;
+        }
+    
+        bodyElement.style.height = this.height + 'px';
+        bodyElement.style.width = this.width + 'px';
+        bodyElement.style.background = this.bg;
+        bodyElement.style.fontSize = this.fontSize + 'px';
 
-DomElement.prototype.createEl = function() {
-    let firstSymbol = this.selector[0],
-        bodyElement;
+        bodyElement.style.position = this.fontSize ? 'relative' : 'absolute';
 
-    if(firstSymbol === '.') {
-        let div = document.createElement('div');
+        return this.selector;
+    };
+    
+    let example = new DomElement('#block', 200, 300, 'transparent', 30);
+    let exampleTwo = new DomElement('.black', 500, 500, 'transparent', 20);
 
-        div.textContent = 'Класс ' + this.selector.slice(1);
-        div.classList.add(this.selector.slice(1))
-        document.body.appendChild(div);
-        bodyElement = div;
-    }
+    let forSquare = new DomElement('.square', 100, 100, '#000', '');
+    
+    let elem = forSquare.createEl();
 
-    if(firstSymbol === '#') {
-        let paragraph = document.createElement('p');
+    document.body.addEventListener('keydown', (e) => {
+        let checkEl = document.querySelectorAll(elem)[0],
+            positionEl;
 
-        paragraph.textContent = 'Айдишник ' + this.selector.slice(1);
-        paragraph.setAttribute('id', this.selector.slice(1));
-        document.body.appendChild(paragraph);
-        bodyElement = paragraph;
-    }
-
-    bodyElement.style.height = this.height + 'px';
-    bodyElement.style.width = this.width + 'px';
-    bodyElement.style.background = this.bg;
-    bodyElement.style.fontSize = this.fontSize + 'px';
-};
-
-let example = new DomElement('#block', 200, 300, '#CCCCCC', 30);
-let exampleTwo = new DomElement('.black', 500, 500, '#f5f5f5', 20);
-
-exampleTwo.createEl();
+        if (e.keyCode === 37) {
+            positionEl = checkEl.style.left === '' ? 0 : checkEl.style.left.slice(0, -2);
+            checkEl.style.left = (+positionEl - 10) + 'px';
+        }
+        if (e.keyCode === 38) {
+            positionEl = checkEl.style.top === '' ? 0 : checkEl.style.top.slice(0, -2);
+            checkEl.style.top = (+positionEl - 10) + 'px';
+        }
+        if (e.keyCode === 39) {
+            positionEl = checkEl.style.left === '' ? 0 : checkEl.style.left.slice(0, -2);
+            checkEl.style.left = (+positionEl + 10) + 'px';
+        }
+        if (e.keyCode === 40) {
+            positionEl = checkEl.style.top === '' ? 0 : checkEl.style.top.slice(0, -2);
+            checkEl.style.top = (+positionEl + 10) + 'px';
+        }
+    });
+});
